dc9f1300f5c9e1e94bb0330fed5bfc66ebb9499b
Taarun Dev <121056618+taarz@users.noreply.github.com>
2023-05-22 21:25:55 +0530

    Create assignment1.m

---
+++ assign1.m
@@ -0,0 +1,39 @@
+% =========================================================
+% Assignment 1: ELEC8540 Automotive Sensor Systems
+% Author: Taarun Dev Karthikeyan
+%
+% Date: 22-05-2022
+% Desc.: To Compare Sobel, Prewitt, Laplacian of Gaussian, 
+% and Canny edge detector on lena image provided.
+% =========================================================
+img = imread('lena.bmp'); % Reading the Image
+noise_var = [0.01, 0.05, 0.1, 0.5, 1]; % Noise variance list
+
+ref_img = edge(img,"Canny",0.1,1); % Reference Image using Canny Edge Detector
+
+for i = 1:5
+    % Adding Gaussian noise to the given Image
+    disp(["Noise_var: ", num2str(noise_var(i))])
+    im = imnoise(img,"gaussian",0,noise_var(i));
+
+    % RMSE of Sobel Edge Detector with noise vs the reference image
+    im1 = edge(im,"Sobel");
+    mse1 = MSE(ref_img,im1);
+    disp(["Sobel", num2str(sqrt(mse1))]);
+
+    % RMSE of Prewitt Edge Detector with noise vs the reference image
+    im2 = edge(im,"Prewitt");
+    mse2 = MSE(ref_img,im2);
+    disp(["Prewitt", num2str(sqrt(mse2))]);
+
+    % RMSE of Laplacian of Gaussian Edge Detector with noise vs the reference image
+    im3 = edge(im,"log");
+    mse3 = MSE(ref_img,im3);
+    disp(["Laplacian of Gaussian", num2str(sqrt(mse3))]);
+
+    % RMSE of Canny Edge Detector with noise vs the reference image
+    im4 = edge(im,"Canny");
+    mse4 = MSE(ref_img,im4);
+    disp(["Canny", num2str(sqrt(mse4))]);
+    
+end
